bac40c2b8e57a499c96fb0b99baa3d499dfbe6ef
Elena Rumiantseva <116489176+elenarumi@users.noreply.github.com>
2025-01-21 15:42:03 +0000

    subscription ejecución

--- matrizCuadrada.java
+++ matrizCuadrada.java
@@ -1,21 +1,21 @@
-package matrizCuadrada
+package matrizCuadrada;
 import java.util.Scanner;
 
-public class cmatrizCuadrada {
+public class matrizCuadrada {
     public static void main(String[] args) {
           int matriz [][] = new int [3][3];
           Scanner teclado = new Scanner(System.in);
 
       
-          for (int f=0; f<matriz.lenght; f++) {
-            for (int c=0; c<matriz[0].lenght; c++){
-              System.out.println("Introduce valor para la fila" + f + "columna" + c);
+          for (int f=0; f < matriz.length; f++) {
+            for (int c=0; c < matriz[0].length; c++){
+              System.out.println("Introduce valor para la fila " + f + "columna " + c);
               matriz[f][c] = teclado.nextInt();
             }
           }
       //recorrido y muestra de los valores
-          for (int f=0; f<matriz.lenght; f++) {
-            for (int c=0; c<matriz[0].lenght; c++){
+          for (int f=0; f < matriz.length; f++) {
+            for (int c=0; c < matriz[0].length; c++){
               System.out.println("Fila:" + f + ";" + "Columna:" + c + ";" + "Valor" + matriz[f][c]);
               matriz[f][c] = teclado.nextInt();
             }
